6afee98e0cf9367c63c587baae9977e046a870f3
Extinthor17 <carle@Carlos-PC>
2023-10-11 12:55:49 +0200

    Pedro Sanchez paga los ERTES cabron

---
+++ Ejercicios 2/src/ej1/Ej1.java
@@ -0,0 +1,14 @@
+package ej1;
+
+public class Ej1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer n = 0;
+		while(n<=5) {
+			System.out.println("Pedro Sanchez cabron paga los ERTES " + n + " aviso.");
+			n++;
+		}
+	}
+
+}
