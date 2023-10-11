2a64854246807706756b1f47287efefa17586412
Extinthor17 <carle@Carlos-PC>
2023-10-11 12:52:23 +0200

    commit 2

--- Prueba jejeje/src/a/A.java
+++ Prueba jejeje/src/a/A.java
@@ -5,6 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Holaaaaaaaaa");
+		System.out.println("Me voy a suicidar");
 	}
 
 }
