ca54515d250fe802ddbcff341775c0e14147a812
Extinthor17 <carle@Carlos-PC>
2023-10-12 13:04:17 +0200

    IF si la eleccion de mano no es correcta

--- Ejercicios Practicos Repaso 2/src/ejj25/Ej25.java
+++ Ejercicios Practicos Repaso 2/src/ejj25/Ej25.java
@@ -6,16 +6,25 @@ import java.util.Scanner;
 public class Ej25 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		Random rand = new Random();
-		System.out.println("Piedra, papel, tijera… un, dos, tres… ya!!");
-		String eleccion = sc.nextLine().toLowerCase();
-		Integer eleccionMaquinaN = rand.nextInt(1,4);
+		String eleccion = "";
+		do {
+
+			System.out.println("Piedra, papel, tijera… un, dos, tres… ya!!");
+			eleccion = sc.nextLine().toLowerCase();
+
+			if (!eleccion.equals("papel") && !eleccion.equals("tijera") && !eleccion.equals("piedra")) {
+				System.out.println("Eleccion no correcta :p");
+			}
+		} while (!eleccion.equals("papel") && !eleccion.equals("tijera") && !eleccion.equals("piedra"));
+
+		Integer eleccionMaquinaN = rand.nextInt(1, 4);
 		String eleccionMaquina = "";
-		
-		switch(eleccionMaquinaN) {
-		
+
+		switch (eleccionMaquinaN) {
+
 		case 1:
 			eleccionMaquina = "piedra";
 			break;
@@ -26,36 +35,31 @@ public class Ej25 {
 			eleccionMaquina = "tijera";
 			break;
 		}
-		
-		System.out.println(eleccionMaquina);
-		
-		if(eleccionMaquina.equals("piedra") && eleccion.equals("piedra")) {
-			System.out.println("EMPATE");
-		}else if(eleccionMaquina.equals("tijera") && eleccion.equals("tijera")) {
-			System.out.println("EMPATE");
-		}
-		else if(eleccionMaquina.equals("papel") && eleccion.equals("papel")) {
-			System.out.println("EMPATE");
-		}
-		else if(eleccionMaquina.equals("piedra") && eleccion.equals("papel")) {
-			System.out.println("GANAS TU!!!!");
-		}else if(eleccionMaquina.equals("piedra") && eleccion.equals("tijera")) {
-			System.out.println("PERDISTE :CCCCC");
-		}
-		else if(eleccionMaquina.equals("papel") && eleccion.equals("piedra")) {
-			System.out.println("PERDISTE :CCCCC");
-		}
-		else if(eleccionMaquina.equals("papel") && eleccion.equals("tijera")) {
-			System.out.println("GANAS TU!!!!");
-		}
-		else if(eleccionMaquina.equals("tijera") && eleccion.equals("papel")) {
-			System.out.println("PERDISTE :CCCCC");
-		}
-		else if(eleccionMaquina.equals("tijera") && eleccion.equals("piedra")) {
-			System.out.println("GANAS TU!!!!");
-		}
-		
 
+		System.out.println(eleccionMaquina);
+
+		if (eleccionMaquina.equals("piedra") && eleccion.equals("piedra")) {
+			System.out.println("EMPATE");
+		} else if (eleccionMaquina.equals("tijera") && eleccion.equals("tijera")) {
+			System.out.println("EMPATE");
+		} else if (eleccionMaquina.equals("papel") && eleccion.equals("papel")) {
+			System.out.println("EMPATE");
+		} else if (eleccionMaquina.equals("piedra") && eleccion.equals("papel")) {
+			System.out.println("GANAS TU!!!!");
+		} else if (eleccionMaquina.equals("piedra") && eleccion.equals("tijera")) {
+			System.out.println("PERDISTE :CCCCC");
+		} else if (eleccionMaquina.equals("papel") && eleccion.equals("piedra")) {
+			System.out.println("PERDISTE :CCCCC");
+		} else if (eleccionMaquina.equals("papel") && eleccion.equals("tijera")) {
+			System.out.println("GANAS TU!!!!");
+		} else if (eleccionMaquina.equals("tijera") && eleccion.equals("papel")) {
+			System.out.println("PERDISTE :CCCCC");
+		} else if (eleccionMaquina.equals("tijera") && eleccion.equals("piedra")) {
+			System.out.println("GANAS TU!!!!");
+		}
+
+		
+		sc.close();
 	}
 
 }
