6d80cec1b6fd4bb92c4e6a6d01e360d47565dbcc
CARLOS SANTOS EXPOSITO <carsanexp@alum.us.es>
2023-10-25 22:51:12 +0200

    ej 9 en transcurso

--- Ejercicios Tema 3-2/src/ejercicio9/Ej9.java
+++ Ejercicios Tema 3-2/src/ejercicio9/Ej9.java
@@ -11,7 +11,6 @@ public class Ej9 {
 		System.out.println("¿Cuantos números vas a meter?");
 		Integer longitudArray = sc.nextInt();
 		Integer arrayNum[] = new Integer[longitudArray];
-		Integer arrayNumSorted[] = new Integer[longitudArray];
 		Integer arrayNumAux[] = new Integer[longitudArray];
 		
 		for (int i = 0; i < arrayNum.length; i++) {
@@ -20,12 +19,22 @@ public class Ej9 {
 			arrayNum[i] = numero;
 			arrayNumAux[i] = numero;
 		}
-		for (int i = 0; i < arrayNumAux.length; i++) {
-			if(arrayNumAux[i+1] < arrayNum[i] && i < arrayNum.length) {
-				arrayNum[i] = arrayNumAux[i+1];
-				arrayNum[i+1] = arrayNum[i];
+		for (int i = 1; i < arrayNumAux.length; i++) {
+			
+				for (int j = 0; j < i; j++) {
+					
+					if(arrayNumAux[i] < arrayNum[j]) {
+						arrayNum[j] = arrayNumAux[i];
+					}else if(j<arrayNum.length - 1){
+						arrayNum[j+1] = arrayNumAux[i];
+					}
 				
 			}
+//			if(arrayNumAux[i+1] < arrayNum[i] && i < arrayNum.length) {
+//				arrayNum[i] = arrayNumAux[i+1];
+//				arrayNum[i+1] = arrayNum[i];
+//				
+//			}
 		}
 		
 		
