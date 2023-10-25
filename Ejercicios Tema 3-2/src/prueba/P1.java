608ddfd1382c74931f9cd52d166dd6b48e0494cb
CARLOS SANTOS EXPOSITO <carsanexp@alum.us.es>
2023-10-25 11:13:09 +0200

    Pruebas

--- Ejercicios Tema 3-2/src/prueba/P1.java
+++ Ejercicios Tema 3-2/src/prueba/P1.java
@@ -1,11 +1,24 @@
 package prueba;
 
+import java.util.Scanner;
+
 public class P1 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce una palabra");
+		String palabra = sc.nextLine();
+		char array1 [] = new char[palabra.length()];
+
 		
+		for(Integer i = 0; i < array1.length; i++) {
+			array1[i]=palabra.charAt(i);
+			System.out.println(array1[i]);
+			
+		}
+		
+		}
 
 	}
 
-}
