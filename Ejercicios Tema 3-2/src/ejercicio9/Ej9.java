efbb4db62087a58eefdf08233124f5e1c625a0d7
Extinthor <csantos6952@ceeuandalucia.es>
2023-10-31 00:12:36 +0100

    A

--- Ejercicios Tema 3-2/src/ejercicio9/Ej9.java
+++ Ejercicios Tema 3-2/src/ejercicio9/Ej9.java
@@ -1,48 +1,49 @@
 package ejercicio9;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ej9 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("¿Cuantos números vas a meter?");
 		Integer longitudArray = sc.nextInt();
+		Integer contador = 0;
 		Integer arrayNum[] = new Integer[longitudArray];
 		Integer arrayNumAux[] = new Integer[longitudArray];
-		
+
 		for (int i = 0; i < arrayNum.length; i++) {
-			System.out.println("Introduce el número " + (i+1));
+			System.out.println("Introduce el número " + (i + 1));
 			Integer numero = sc.nextInt();
 			arrayNum[i] = numero;
 			arrayNumAux[i] = numero;
 		}
 		for (int i = 1; i < arrayNumAux.length; i++) {
-			
-				for (int j = 0; j < i; j++) {
+
+			for (int j = 0; j < i; j++) {
+
+				if (arrayNumAux[i] < arrayNum[j]) {
+					arrayNum[j] = arrayNumAux[i];
+					arrayNum[j+1] = arrayNumAux[i-1];
 					
-					if(arrayNumAux[i] < arrayNum[j]) {
-						arrayNum[j] = arrayNumAux[i];
-					}else if(j<arrayNum.length - 1){
-						arrayNum[j+1] = arrayNumAux[i];
-					}
-				
+					contador++;
+				} else if (j < arrayNum.length - 1) {
+					Integer previo =  arrayNum[i-1];
+					arrayNum[j + 1] = arrayNumAux[i];
+					arrayNum[j + 2] = previo;
+					
+					
+					
+				}
+
 			}
-//			if(arrayNumAux[i+1] < arrayNum[i] && i < arrayNum.length) {
-//				arrayNum[i] = arrayNumAux[i+1];
-//				arrayNum[i+1] = arrayNum[i];
-//				
-//			}
 		}
-		
-		
-		
+
 		for (int i = 0; i < arrayNum.length; i++) {
 			System.out.print(arrayNum[i] + " ");
 		}
-
+		sc.close();
 	}
 
 }
