4390090f86778e80ea6c83d922d2a9442aab4a3b
TECH <TECH@CEP-INFOR6-PC07.andalucia.fusp.ceu>
2023-10-31 15:10:59 +0100

    FIN DEL 9 POR COMPLETOOOO

--- Ejercicios Tema 3-2/src/ejercicio9/Ej9V2.java
+++ Ejercicios Tema 3-2/src/ejercicio9/Ej9V2.java
@@ -1,50 +1,39 @@
 package ejercicio9;
 
-	import java.util.Scanner;
+import java.util.Scanner;
 
-	public class Ej9V2 {
+public class Ej9V2 {
 
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 
-			Scanner sc = new Scanner(System.in);
-			System.out.println("¿Cuantos números vas a meter?");
-			Integer longitudArray = sc.nextInt();
-			Integer contador = 0;
-			Integer arrayNum[] = new Integer[longitudArray];
-			Integer arrayNumAux[] = new Integer[longitudArray];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("¿Cuantos números vas a meter?");
+		Integer longitudArray = sc.nextInt();
+		Integer contador = 0;
+		Integer arrayNum[] = new Integer[longitudArray];
 
-			for (int i = 0; i < arrayNum.length; i++) {
-				System.out.println("Introduce el número " + (i + 1));
-				Integer numero = sc.nextInt();
-				arrayNum[i] = numero;
-				arrayNumAux[i] = numero;
-			}
-			for (int i = 1; i < arrayNumAux.length; i++) {
+		for (int i = 0; i < arrayNum.length; i++) {
+			System.out.println("Introduce el número " + (i + 1));
+			Integer numero = sc.nextInt();
+			arrayNum[i] = numero;
+		}
+		for (int i = 0; i < arrayNum.length; i++) {
 
-				for (int j = 0; j < i; j++) {
-
-					if (arrayNumAux[i] < arrayNum[j]) {
-						arrayNum[j] = arrayNumAux[i];
-						arrayNum[j+1] = arrayNumAux[i-1];
-						
-						contador++;
-					} else if (j < arrayNum.length - 1) {
-						Integer previo =  arrayNum[i-1];
-						arrayNum[j + 1] = arrayNumAux[i];
-						arrayNum[j + 2] = previo;
-						
-						
-						
-					}
-
+			for (int j = i + 1; j < arrayNum.length; j++) {
+				
+				if(arrayNum[j] < arrayNum[i]) {
+					Integer aux = arrayNum[i];
+					arrayNum[i] = arrayNum[j];
+					arrayNum[j] = aux;
 				}
 			}
 
-			for (int i = 0; i < arrayNum.length; i++) {
-				System.out.print(arrayNum[i] + " ");
-			}
-			sc.close();
 		}
 
+		for (int i = 0; i < arrayNum.length; i++) {
+			System.out.print(arrayNum[i] + " ");
+		}
+		sc.close();
 	}
 
+}
